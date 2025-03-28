ece654c18c8249812be7bf83f86af70d52ae81bb
javidominguezsegura <javier.dominguez.segura@gmail.com>
2025-03-28 21:05:48 +0100

    corrected some bug that made val and test give the same results!

--- 1st_Iteration/utilities_functions.py
+++ 1st_Iteration/utilities_functions.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.metrics import accuracy_score, roc_curve, roc_auc_score, confusion_matrix, classification_report
 
 
 def compute_correlation_matrix(figsize: tuple[int, int], dataframe: pd.DataFrame):
@@ -78,3 +79,19 @@ def get_y_sets_encoded(y_train: pd.Series, y_val: pd.Series, y_test: pd.Series) 
   y_test_encoded = pd.Series(labeller.transform(y_test), index=y_test.index)
 
   return y_train_encoded, y_val_encoded, y_test_encoded
+
+# def evaluate_classifier(y_actual: pd.Series, y_pred: pd.Series, display_results: bool = False, plot_confusion_matrix: bool = False) -> tuple[float, str, np.ndarray]:
+#   """ Returns commonc classification metrics, plots confusion matrix if requested, writes to stdout if requested"""
+#   val_acc = accuracy_score(y_actual, y_pred)
+#   class_report = classification_report(y_actual, y_pred) # F1 score, precision, recall for each class
+#   conf_matrix = confusion_matrix(y_actual, y_pred)
+#   if display_results:
+#     print(f"Validation Accuracy: {val_acc}")
+#     print(f"Validation Classification Report: {class_report}")
+#   if plot_confusion_matrix:
+#     sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues')
+#     plt.xlabel("Predicted Label")
+#     plt.ylabel("True Label")
+#     plt.title("Confusion Matrix")
+#     plt.show()
+#   return val_acc, class_report, conf_matrix
