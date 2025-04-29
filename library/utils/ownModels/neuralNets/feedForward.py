58ca7b48a4c6e775c84261814810e3355ea924c5
javidominguezsegura <javier.dominguez.segura@gmail.com>
2025-04-29 17:59:56 +0200

    neural nets evaluation going perfect

--- library/utils/ownModels/neuralNets/feedForward.py
+++ library/utils/ownModels/neuralNets/feedForward.py
@@ -2,8 +2,9 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.utils import to_categorical
-import numpy as np
+from tensorflow.keras.callbacks import EarlyStopping
 
+import numpy as np
 class FeedForwardNeuralNetwork():
       """
       Defines a standard non-optimized architecture 
@@ -21,8 +22,8 @@ class FeedForwardNeuralNetwork():
       def _build_model(self):
             model = Sequential([
                   tf.keras.Input(shape=(self.X_shape[1],)),  # Explicit Input layer
-                  Dense(32, activation='relu', kernel_initializer='glorot_uniform'),
-                  Dense(32, activation='relu', kernel_initializer='glorot_uniform'),
+                  Dense(1, activation='relu', kernel_initializer='glorot_uniform'),
+                  Dense(1, activation='relu', kernel_initializer='glorot_uniform'),
                   Dense(self.y_shape[0], activation='softmax', kernel_initializer='glorot_uniform')  # Softmax for multiclass classification
             ])
             model.compile(
@@ -34,17 +35,30 @@ class FeedForwardNeuralNetwork():
             return model
       
       def fit(self, X_data, y_data, X_val, y_val):
+
+            early_stop = EarlyStopping(
+                  monitor='val_loss',     
+                  patience=3,             
+                  restore_best_weights=True,  
+                  verbose=3
+            )
+            
             history = self.model.fit(X_data, 
                            y_data, 
                            epochs=2,
-                           batch_size=32, 
-                           validation_data=(X_val, y_val))
+                           batch_size=5, 
+                           validation_data=(X_val, y_val),
+                           callbacks=[early_stop])
+            
+
             self.history = history
             return self, history
       
       def predict(self, X_data):
             self.soft_predictions = self.model.predict(X_data)
+            print(f"Soft predictions done")
             self.hard_predictions = np.argmax(self.soft_predictions, axis=1)
+            print(f"Hard predictions done")
             return self.hard_predictions
       
       def get_params(self):
